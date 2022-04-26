7e611e56f5ba8b4bf392f729ac92c7f84713f60e
leonardo1313 <leonardo.novak5@gmail.com>
2022-04-26 20:47:12 +0200

    Initial commit

---
+++ app/src/main/java/com/optimahorizonapps/buttoncounterapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.optimahorizonapps.buttoncounterapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private var userInput: EditText? = null
+    private var button: Button? = null
+    private var textView: TextView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        userInput = findViewById(R.id.editText)
+        button = findViewById(R.id.button)
+        textView = findViewById(R.id.textView)
+
+    }
+}

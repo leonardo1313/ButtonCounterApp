ad5a1dc60fa329b9132eaedd9191f305fd817ab9
leonardo1313 <leonardo.novak5@gmail.com>
2022-04-27 20:10:31 +0200

    - activity lifecycle methods crated - TextView text saved when screen rotates
    App finished

--- app/src/main/java/com/optimahorizonapps/buttoncounterapp/MainActivity.kt
+++ app/src/main/java/com/optimahorizonapps/buttoncounterapp/MainActivity.kt
@@ -3,39 +3,81 @@ package com.optimahorizonapps.buttoncounterapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
+private const val TAG = "MainActivity"
+private const val TEXT_CONTENT = "TextContent"
+
 class MainActivity : AppCompatActivity() {
 
-    private var userInput: EditText? = null
-    private var button: Button? = null
     private var textView: TextView? = null
-    private var numTimesClicked = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate: called")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        userInput = findViewById(R.id.editText)
-        button = findViewById(R.id.button)
+        val userInput: EditText = findViewById(R.id.editText)
+        val button: Button = findViewById(R.id.button)
         textView = findViewById(R.id.textView)
         textView?.text = ""
         textView?.movementMethod = ScrollingMovementMethod()
+        userInput.setText("")
 
-        button?.setOnClickListener(object : View.OnClickListener{
+        button.setOnClickListener(object : View.OnClickListener {
             override fun onClick(p0: View?) {
-                numTimesClicked += 1
-                textView?.append("The button was pressed $numTimesClicked time")
-                if(numTimesClicked != 1) {
-                    textView?.append("s\n")
-                } else {
-                    textView?.append("\n")
-                }
+                Log.d(TAG, "onClick: called")
+                textView?.append(userInput.text.toString() + "\n")
+                userInput.text.clear()
+
             }
         })
+    }
 
+    override fun onStart() {
+        Log.d(TAG, "onStart: called")
+        super.onStart()
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        Log.d(TAG, "onRestoreInstanceState: called")
+        super.onRestoreInstanceState(savedInstanceState)
+        textView?.text = savedInstanceState.getString(TEXT_CONTENT, "")
+    }
+
+    override fun onResume() {
+        Log.d(TAG, "onResume: called")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d(TAG, "onPause: called")
+        super.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        Log.d(TAG, "onSaveInstanceState: called")
+        super.onSaveInstanceState(outState)
+        outState.putString(TEXT_CONTENT, textView?.text.toString())
+    }
+
+    override fun onStop() {
+        Log.d(TAG, "onStop: called")
+        super.onStop()
+    }
+
+    override fun onRestart() {
+        Log.d(TAG, "onRestart: called")
+        super.onRestart()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG, "onDestroy: called")
+        super.onDestroy()
     }
 }
